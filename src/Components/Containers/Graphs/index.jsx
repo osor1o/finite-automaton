ccba6f14f91704280d4dbf49d63014e268c1b1c9
osor1o-pc <osorioneto97@gmail.com>
2020-05-13 20:15:05 -0300

    highlight graphs

--- src/Components/Containers/Graphs/index.jsx
+++ src/Components/Containers/Graphs/index.jsx
@@ -5,13 +5,13 @@ import { v4 as uuidv4 } from 'uuid'
 
 const graph = {
   nodes: [
-    { id: 1, label: "Node 1", title: "node 1 tootip text" },
-    { id: 2, label: "Node 2", title: "node 2 tootip text" },
-    { id: 3, label: "Node 3", title: "node 3 tootip text" },
-    { id: 4, label: "Node 4", title: "node 4 tootip text" },
+    { id: 1, label: "Node 1", selected: true, color: { border: '#7159c1', }, borderWidth: 2 },
+    { id: 2, label: "Node 2", },
+    { id: 3, label: "Node 3", },
+    { id: 4, label: "Node 4", },
   ],
   edges: [
-    { from: 1, to: 2, label: '123' },
+    { from: 1, to: 2, label: '123', width: 2 },
     { from: 1, to: 1, label: '123' },
     { from: 1, to: 3 },
     { from: 2, to: 4 },
