0cc8610a99a92770b6d0de266d54d0efe47635b1
osor1o-pc <osorioneto97@gmail.com>
2020-05-14 10:38:51 -0300

    Updates

---
+++ precache-manifest.56dac62b285a0265260c1373d13d8b47.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b1302b48b9fbc4726706a71338f41955",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0b876c60c745b4b89e00",
+    "url": "/static/js/2.564d959b.chunk.js"
+  },
+  {
+    "revision": "5ce43066b62c06c3dc78b6f331d6696a",
+    "url": "/static/js/2.564d959b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "229a46ebbb54aa44aa9a",
+    "url": "/static/js/main.feba345c.chunk.js"
+  },
+  {
+    "revision": "169b52a2a92bd06f0782",
+    "url": "/static/js/runtime-main.1fec9604.js"
+  }
+]);
